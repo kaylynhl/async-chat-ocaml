64da9b4f49e2b5c71c45a6b0939d1d6405a5f1f1
Kaylyn Lee <khl62@cornell.edu>
2024-11-26 17:31:19 -0500

    Implement group chat and client and server loops
    - Allow multiple messages to be sent and received

--- bin/main.ml
+++ bin/main.ml
@@ -3,28 +3,60 @@ let string_of_sockaddr = function
   | ADDR_INET (ip, port) ->
       Printf.sprintf "%s:%d" (Unix.string_of_inet_addr ip) port
 
+let clients_lst = ref []
+
+let broadcast sender msg =
+  Lwt_list.iter_p
+    (fun (_, client_out) ->
+      if client_out != sender then Lwt_io.write_line client_out msg
+      else Lwt.return_unit)
+    !clients_lst
+
 let client_handler client_socket_address (client_in, client_out) =
   let%lwt () =
     Lwt_io.printlf "I got a connection from %s."
       (string_of_sockaddr client_socket_address)
   in
-  let%lwt msg = Lwt_io.read_line client_in in
+
+  let%lwt username = Lwt_io.read_line client_in in
+  clients_lst := (client_in, client_out) :: !clients_lst;
+
   let%lwt () =
-    Lwt_io.printlf "Message received from client %s. %s"
-      (string_of_sockaddr client_socket_address)
-      msg
+    broadcast client_out (Printf.sprintf "%s has entered the chat." username)
   in
-  Lwt.return ()
+
+  let rec client_msg_loop () =
+    try%lwt
+      let%lwt msg = Lwt_io.read_line client_in in
+      let message = Printf.sprintf "%s: %s" username msg in
+      let%lwt () = broadcast client_out message in
+      client_msg_loop ()
+    with
+    | End_of_file ->
+        let%lwt () =
+          Lwt_io.printlf "Client %s has disconnected."
+            (string_of_sockaddr client_socket_address)
+        in
+        clients_lst :=
+          List.filter (fun (_, co) -> co != client_out) !clients_lst;
+        Lwt.return ()
+    | _ ->
+        let%lwt () = Lwt_io.printlf "Error occurred with handling client." in
+        Lwt.return ()
+  in
+  client_msg_loop ()
 
 let run_server addr =
   let server () =
-    let%lwt () =
-      Lwt_io.printlf "I am the server on %s." (string_of_sockaddr addr)
-    in
-    let%lwt running_server =
-      Lwt_io.establish_server_with_client_address addr client_handler
-    in
-    fst (Lwt.wait ())
+    try%lwt
+      let%lwt () =
+        Lwt_io.printlf "I am the server on %s." (string_of_sockaddr addr)
+      in
+      let%lwt running_server =
+        Lwt_io.establish_server_with_client_address addr client_handler
+      in
+      fst (Lwt.wait ())
+    with _ -> Lwt.return ()
   in
   Lwt_main.run (server ())
 
@@ -36,8 +68,31 @@ let run_client addr username =
     in
     let%lwt server_in, server_out = Lwt_io.open_connection addr in
     let%lwt () = Lwt_io.printlf "I connected to the server." in
-    let%lwt msg = Lwt_io.read_line Lwt_io.stdin in
-    let%lwt () = Lwt_io.write_line server_out msg in
+
+    let%lwt () = Lwt_io.write_line server_out username in
+
+    (* loop to send messages *)
+    let rec client_msg_loop () =
+      let%lwt msg = Lwt_io.read_line Lwt_io.stdin in
+      let%lwt () = Lwt_io.write_line server_out msg in
+      client_msg_loop ()
+    in
+
+    (* loop to get messages *)
+    let rec client_get_loop () =
+      try%lwt
+        let%lwt msg = Lwt_io.read_line server_in in
+        let%lwt () = Lwt_io.printlf "%s" msg in
+        client_get_loop ()
+      with
+      | End_of_file ->
+          let%lwt () = Lwt_io.printlf "Disconnected from the server." in
+          Lwt.return_unit
+      | _ ->
+          let%lwt () = Lwt_io.printlf "Error with receiving messages." in
+          Lwt.return_unit
+    in
+    let%lwt () = Lwt.pick [ client_msg_loop (); client_get_loop () ] in
     Lwt.return ()
   in
   Lwt_main.run (client ())
